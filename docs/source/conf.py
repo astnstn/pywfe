a33fa9b5e4149db25cd297165c91fc4e409ca288
Austen <austenkentellstone@gmai.com>
2024-05-24 18:47:51 +0100

    conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
-
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -21,22 +20,21 @@ project = 'pywfe'
 copyright = '2023, Austen Stone'
 author = 'Austen Stone'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ["numpydoc", "sphinx.ext.autodoc", "sphinx.ext.autosummary"]
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = [
+    "numpydoc",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary"
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+# List of patterns, relative to source directory, that match files and directories to ignore when looking for source files.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -45,18 +43,31 @@ exclude_patterns = []
 #
 html_theme = 'alabaster'
 
+# Generate autosummary pages
 autosummary_generate = True
+
+# Control the order of members in autodoc
 autodoc_member_order = 'bysource'
+
+# Control whether numpydoc should include a class members toctree
 numpydoc_class_members_toctree = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-##############################
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+# Autodoc options
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'private-members': False,
+    'show-inheritance': True,
+    'inherited-members': True,
+    'special-members': '__init__',
+    'exclude-members': '__weakref__',
+    'noindex': True,  # This prevents individual functions from appearing in the index
+}
 
 # --- OPTIONS FOR LATEX
 
@@ -64,6 +75,5 @@ fh = open('latex_preamble.tex', 'r+')
 PREAMBLE = fh.read()
 fh.close()
 latex_elements = {
-# Additional stuff for the LaTeX preamble.
-'preamble': PREAMBLE,
+    'preamble': PREAMBLE,
 }
