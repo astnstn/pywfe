f10b50557cb870fd44616e1521111740e1b066ff
Austen <austenkentellstone@gmai.com>
2024-05-24 18:15:23 +0100

    conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../..'))  # Adjust this if necessary
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
